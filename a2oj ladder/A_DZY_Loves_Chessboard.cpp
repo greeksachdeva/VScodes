e6c890c3d6bbab0df3d754a5e81789d9f2e961cb
Greek Sachdeva <greeksachdeva@gmail.com>
2021-11-17 01:23:58 +0530

    .

---
+++ a2oj ladder/A_DZY_Loves_Chessboard.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define fo(i,n) for(ll i=0;i<n;i++)
+#define fo1(i,n) for(ll i=1;i<=n;i++)
+#define all(v) v.begin(), v.end()
+#define sortall(v) sort(all(v))
+#define sortalld(v) sort(all(v),greater<ll>())
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int> veci;
+typedef vector<ll>  vecll;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+const int mod = 1000000007;
+int pow(int base, int exp) {base %= mod;int result = 1;while (exp > 0) {if (exp & 1) result = ((ll)result * base) % mod;base = ((ll)base * base) % mod;exp >>= 1;}return result;}
+int T, P, Q, R, X, Y, U, V, N,M;
+ll t, p, q, r, x, y, u, v, n, m,k;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin >> n >> m;
+    vector<vector<char>> v(n, vector<char>(m));
+    fo(i, n)
+    {
+        fo(j, m)
+        {
+            cin >> v[i][j];
+        }
+    }
+    fo(i,n)
+    {
+        fo(j,m)
+        {
+            if(i%2==0)
+            {
+                if(v[i][j]=='.' && j%2==0)
+                {
+                    v[i][j] = 'B';
+                }
+                else if(v[i][j]=='.' && j%2==1)
+                {
+                    v[i][j] = 'W';
+                }
+            }
+            if(i%2==1)
+            {
+                if(v[i][j]=='.' && j%2==0)
+                {
+                    v[i][j] = 'W';
+                }
+                else if(v[i][j]=='.' && j%2==1)
+                {
+                    v[i][j] = 'B';
+                }
+            }
+        }
+    }
+    fo(i,n)
+    {
+        fo(j,m)
+        {
+            cout<<v[i][j];
+        }
+        cout << endl;
+    }
+    }
+// string.find("xyz") !=string::npos  //if xyz is found
+
+
